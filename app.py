4e5e50baeadcd1c7588184efa35d90f1257dbc78
Bamigboye Oluwatimilehin Opeoluwa <45441071+Timchester27@users.noreply.github.com>
2022-03-02 11:29:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ st.pyplot(fig2)
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://wallpaperaccess.com/full/1393720.jpg");
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
 </style>
